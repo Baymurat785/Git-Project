d393667f938f783a27780340b68506666b62e75c
Baymurat785 <123241080+Baymurat785@users.noreply.github.com>
2024-06-09 06:28:52 +0500

    Many changes have been made

---
+++ Git Project/ModelView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelView.swift
+//  Git Project
+//
+//  Created by Baymurat Abdumuratov on 09/06/24.
+//
+
+import SwiftUI
+
+struct ModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelView()
+}
